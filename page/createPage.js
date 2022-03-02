45b05f94b3398160137697c516bcc58cf3751281
CaduPinheiro <99294607+CaduPinheiro@users.noreply.github.com>
2022-03-02 20:22:44 -0300

    Update createPage.js

--- page/createPage.js
+++ page/createPage.js
@@ -13,23 +13,23 @@ function page(){
                         </div>
                         <div>
                             ${noticia.subtitle !== "" ? `<span>${noticia.subtitle}</span>` : ""}
-                            <h2 class="title">${noticia.title}</h2>
+                            <h2 class="title"<a href="${noticia.noticeLink}" target="_blank">${noticia.title}</h2>
                             <ul class="ul-relatedItems">
                                 ${noticia.relatedItems.length > 0 ?
                                     noticia.relatedItems.length === 1 ? `
                                         <li>
-                                            <a href="${noticia.relatedItems[0].relatedLink}" class="subItem">
+                                            <a href="${noticia.relatedItems[0].relatedLink}" class="subItem" target="_blank">
                                                 ${noticia.relatedItems[0].relatedTitle}
                                             </a>
                                         </li>
                                     `:`
                                         <li>
-                                            <a href="${noticia.relatedItems[0].relatedLink}" class="subItem">
+                                            <a href="${noticia.relatedItems[0].relatedLink}" class="subItem" target="_blank">
                                                 ${noticia.relatedItems[0].relatedTitle}
                                             </a>
                                         </li>
                                         <li>
-                                            <a href="${noticia.relatedItems[1].relatedLink}" class="subItem">
+                                            <a href="${noticia.relatedItems[1].relatedLink}" class="subItem" target="_blank">
                                                 ${noticia.relatedItems[1].relatedTitle}
                                             </a>
                                         </li>
